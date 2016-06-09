7cc945095f2bf2bab371d6d0247b4dab68b89ea8
Dennis Mantz <dennis.mantz@googlemail.com>
2016-06-09 02:54:55 +0200

    Adding RDS decoding for groups 0A,0B,2A,2B.

--- app/src/main/java/de/tu/darmstadt/seemoo/ansian/model/demodulation/RDS.java
+++ app/src/main/java/de/tu/darmstadt/seemoo/ansian/model/demodulation/RDS.java
@@ -4,6 +4,7 @@ import android.provider.Settings;
 import android.util.Log;
 
 import de.tu.darmstadt.seemoo.ansian.model.SamplePacket;
+import de.tu.darmstadt.seemoo.ansian.model.preferences.FloatEditTextPreference;
 
 public class RDS extends Demodulation {
     private static final String LOGTAG = "RDS";
@@ -25,9 +26,58 @@ public class RDS extends Demodulation {
     private static final int CHECKWORDSIZE = 10;
     private static final int BLOCKSIZE_W_CHECKWORD = BLOCKSIZE + CHECKWORDSIZE;
 
+    private static final String[] programTypeString = {     "L", "I", "N", "S", "R1", "R2", "R3", "R4", "R5",
+                                                            "R6", "R7", "R8", "R9", "R10", "R11", "R12"};
+    private static final String[] programTypeCodeString = { "None", "News", "Current Affairs", "Information",
+                                                            "Sport", "Education", "Drama", "Cultures", "Science",
+                                                            "Varied Speech", "Pop Music", "Rock Music", "Easy Listening",
+                                                            "Light Classics M", "Serious Classics", "Other Music"};
+
+    // Current state
+    private int countryCode;
+    private int programType;
+    private int programReferenceNumber;
+    private int TP;
+    private int programTypeCode;
+    private int TA;
+    private int MS;
+    private int stereo;
+    private int artificialHead;
+    private int compressed;
+    private int dynPTY;
+    private float altFreq1;
+    private float altFreq2;
+    private char[] programName;
+    private int textAB;
+    private char[] radioText;
+
     public RDS() {
         bitBuffer = new byte[2000];     // Just a large buffer, no need for optimization here..
         bpsk = new BPSK(BAUDRATE);
+        resetState();
+    }
+
+    private void resetState() {
+        countryCode = -1;
+        programType = -1;
+        programReferenceNumber = -1;
+        TP = -1;
+        programTypeCode = -1;
+        TA = -1;
+        MS = -1;
+        stereo = -1;
+        artificialHead = -1;
+        compressed = -1;
+        dynPTY = -1;
+        altFreq1 = Float.NaN;
+        altFreq2 = Float.NaN;
+        textAB = -1;
+        programName = new char[8];
+        radioText = new char[64];
+        for(int i = 0; i < programName.length; i++)
+            programName[i] = ' ';
+        for(int i = 0; i < radioText.length; i++)
+            radioText[i] = ' ';
     }
 
     @Override
@@ -35,27 +85,47 @@ public class RDS extends Demodulation {
         // Demodulate the new samples
         bitBufferSize += bpsk.demodulate(input, bitBuffer, bitBufferSize);
 
-        while(bitBufferSize-bitBufferStart >= BLOCKSIZE_W_CHECKWORD) {
+        while(bitBufferSize-bitBufferStart >= BLOCKSIZE_W_CHECKWORD*4) {
             // Try to decode the bits (check the checksum)
             int blocktype = checkBits(bitBuffer, bitBufferStart);
 
-            // DEBUG:
+            // Continue with the next bit if checkword does not match
             if (blocktype < 0) {
                 bitBufferStart++;
                 continue;
             }
 
-            System.out.printf("demodulate: FOUND BLOCK %s! : ", BLOCK_TYPE_NAME[blocktype]);
-            for(int i = 0; i < BLOCKSIZE; i++)
-                System.out.printf("%d", bitBuffer[bitBufferStart+i]);
-            System.out.printf("\n");
+            // If we found a BLOCK A, we can decode it on its own (it is independend of the rest of the group:
+            if(blocktype == BLOCKA) {
+                String logtext = decodePI(bitBuffer, bitBufferStart);
+                bitBufferStart += BLOCKSIZE_W_CHECKWORD;
+                Log.i(LOGTAG, "demodulate: Found Block A " + logtext);
+                continue;
+            }
 
+            // check if we found a decodeable group (Block B followed by C and D):
+            if(blocktype == BLOCKB
+                    &&  checkBits(bitBuffer, bitBufferStart+BLOCKSIZE_W_CHECKWORD)   == BLOCKC
+                    &&  checkBits(bitBuffer, bitBufferStart+BLOCKSIZE_W_CHECKWORD*2) == BLOCKD) {
+                // Found a decodeable group!
+                String logtext = decodeGroup(bitBuffer, bitBufferStart);
+                Log.i(LOGTAG, "demodulate: " + logtext);
+                Log.d(LOGTAG, "domodulate: ProgName="+new String(programName)+" RadioText="+new String(radioText));
 
-            // Do decoding and process the information... (TODO)
+                // Next group:
+                bitBufferStart += BLOCKSIZE_W_CHECKWORD*3;
+            } else {
+                // Found a block which is not decodeable on its own:
+                /*
+                System.out.printf("demodulate: Found BLOCK %s! outside of group : ", BLOCK_TYPE_NAME[blocktype]);
+                for(int i = 0; i < BLOCKSIZE; i++)
+                    System.out.printf("%d", bitBuffer[bitBufferStart+i]);
+                System.out.printf("\n");
+                */
 
-
-            // Next packet:
-            bitBufferStart += BLOCKSIZE_W_CHECKWORD;
+                // Next bit
+                bitBufferStart++;
+            }
         }
 
         // Check if we have to rearrange the bitbuffer:
@@ -125,19 +195,130 @@ public class RDS extends Demodulation {
         return INVALIDBLOCK;
     }
 
-    public void decodeBlockA(byte[] bits) {
-
+    private int bits2Int(byte[] bits, int start, int len) {
+        int value = 0;
+        for(int i = 0; i < len; i++)
+            if(bits[start+i] > 0)
+                value += Math.pow(2,len-i-1);
+        return value;
     }
 
-    public byte decodeBlockB(byte[] bits) {
-        return 0x00;
+    public String decodePI(byte[] bits, int idx) {
+        countryCode = bits2Int(bits, idx, 4);
+        programType = bits2Int(bits, idx+4, 4);
+        programReferenceNumber = bits2Int(bits, idx+8, 8);
+
+        return String.format("PI=[CC=0x%X PT=%s PRN=0x%02X]",
+                countryCode, programTypeString[programType], programReferenceNumber);
     }
 
-    public void decodeBlockC(byte[] bits) {
+    public String decodeGroup(byte[] bits, int idx) {
+        int groupTypeCode = bits2Int(bits, idx, 4);
+        char groupVersion = bits[idx+4] == 0 ? 'A' : 'B';
+        TP = bits[idx+5];
+        programTypeCode = bits2Int(bits, idx+6, 4);
 
+        String logtext = String.format("Group %d%c TP=%b PTY=0x%X(%s) ", groupTypeCode,
+                groupVersion, TP, programTypeCode, programTypeCodeString[programTypeCode]);
+
+        switch (groupTypeCode) {
+            case 0:
+                logtext += decodeType0(bits, idx, groupVersion);
+                break;
+
+            case 2:
+                logtext += decodeType2(bits, idx, groupVersion);
+                break;
+
+            default:
+                break;
+        }
+        return logtext;
     }
 
-    public void decodeBlockD(byte[] bits) {
+    public String decodeType0(byte[] bits, int idx, char groupVersion) {
+        int idxC = idx+BLOCKSIZE_W_CHECKWORD;
+        int idxD = idx+BLOCKSIZE_W_CHECKWORD*2;
 
+        TA = bits[idx+11];
+        MS = bits[idx+12];
+
+        String logtext = "TA="+TA+" MS="+MS;
+
+        int segmentAddressCode = bits2Int(bits, idx+14, 2);
+        switch (segmentAddressCode) {
+            case 0:
+                stereo = bits[idx+13];
+                logtext += " stereo="+stereo;
+                break;
+            case 1:
+                artificialHead = bits[idx+13];
+                logtext += " artificialHead="+artificialHead;
+                break;
+            case 2:
+                compressed = bits[idx+13];
+                logtext += " compressed="+compressed;
+                break;
+            case 3:
+                dynPTY = bits[idx+13];
+                logtext += " dynPTY="+dynPTY;
+                break;
+        }
+
+        if(groupVersion == 'B') {
+            logtext += " " + decodePI(bits, idxC); // C is PI
+        } else {
+            // C is alt. frequencies
+            int rawFreq1 = bits2Int(bits, idxC, 8);
+            int rawFreq2 = bits2Int(bits, idxC+8, 8);
+            if(rawFreq1<205) {
+                altFreq1 = 87.5f + 0.1f * rawFreq1;
+                logtext += " altFreq1="+altFreq1;
+            }
+            if(rawFreq2<205) {
+                altFreq2 = 87.5f + 0.1f *rawFreq2;
+                logtext += " altFreq2="+altFreq2;
+            }
+        }
+
+        char character1 = (char) bits2Int(bits, idxD, 8);
+        char character2 = (char) bits2Int(bits, idxD+8, 8);
+        logtext += " ProgName["+segmentAddressCode*2+","+(segmentAddressCode*2+1)+"]="+character1+character2;
+        programName[segmentAddressCode*2] = character1;
+        programName[segmentAddressCode*2+1] = character2;
+
+        return logtext;
+    }
+
+    public String decodeType2(byte[] bits, int idx, char groupVersion) {
+        int idxC = idx+BLOCKSIZE_W_CHECKWORD;
+        int idxD = idx+BLOCKSIZE_W_CHECKWORD*2;
+
+        textAB = bits[idx+11];
+        String logtext = "txtAB="+textAB;
+
+        int segmentAddressCode = bits2Int(bits, idx+12, 4);
+        char characterD1 = (char) bits2Int(bits, idxD, 8);
+        char characterD2 = (char) bits2Int(bits, idxD+8, 8);
+
+        if(groupVersion == 'B') {
+            logtext += " " + decodePI(bits, idxC); // C is PI
+
+            logtext += " RadioText["+segmentAddressCode*2+","+(segmentAddressCode*2+1)+"]="+characterD1+characterD2;
+            radioText[segmentAddressCode*2] = characterD1;
+            radioText[segmentAddressCode*2+1] = characterD2;
+        } else {
+            // C is radio text
+            char characterC1 = (char) bits2Int(bits, idxC, 8);
+            char characterC2 = (char) bits2Int(bits, idxC+8, 8);
+            logtext += " RadioText["+segmentAddressCode*4+","+(segmentAddressCode*4+1)+","+(segmentAddressCode*4+2)+
+                    ","+(segmentAddressCode*4+3)+"]="+characterC1+characterC2+characterD1+characterD2;
+            radioText[segmentAddressCode*4]   = characterC1;
+            radioText[segmentAddressCode*4+1] = characterC2;
+            radioText[segmentAddressCode*4+2] = characterD1;
+            radioText[segmentAddressCode*4+3] = characterD2;
+        }
+
+        return logtext;
     }
 }
