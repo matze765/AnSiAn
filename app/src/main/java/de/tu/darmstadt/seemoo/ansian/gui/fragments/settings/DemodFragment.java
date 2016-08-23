dd5e6c8af934fc1868e4f80311e4a5c3ae610a84
Synisysm <synic@synical.de>
2016-08-23 13:17:48 +0200

    disable logfile selection if logging is disabled

--- app/src/main/java/de/tu/darmstadt/seemoo/ansian/gui/fragments/settings/DemodFragment.java
+++ app/src/main/java/de/tu/darmstadt/seemoo/ansian/gui/fragments/settings/DemodFragment.java
@@ -1,36 +1,62 @@
 package de.tu.darmstadt.seemoo.ansian.gui.fragments.settings;
 
 import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.preference.EditTextPreference;
 import android.preference.ListPreference;
 import android.preference.Preference;
 import android.preference.SwitchPreference;
+
 import de.tu.darmstadt.seemoo.ansian.model.preferences.Preferences;
 
 /**
  * Fragment for Morse preferences
- *
  */
 public class DemodFragment extends MyPreferenceFragment {
 
-	private static final String LOGTAG = "DemodFragment";
-	
-	public DemodFragment() {
-		super(Preferences.DEMOD_PREFERENCE);
-		}
+    private static final String LOGTAG = "DemodFragment";
 
-	@Override
-	public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
-		super.onSharedPreferenceChanged(sharedPreferences, key);	
-		Preference pref = findPreference(key);
+    public DemodFragment() {
+        super(Preferences.DEMOD_PREFERENCE);
+    }
 
-		// disable automatic reinit if manual mode is selected
-		if (key.equals("receive_mode")) {
-			ListPreference listPref = (ListPreference) pref;
-			SwitchPreference switchPref= (SwitchPreference) findPreference("automatic_init");
-			if(listPref.getValue().equals("1"))
-				switchPref.setEnabled(false);
-			else
-				switchPref.setEnabled(true);
-		}
-	}
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        updateReinitEnabledState();
+        updateLogfileEnabledState();
+    }
+
+    @Override
+    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
+        super.onSharedPreferenceChanged(sharedPreferences, key);
+        if (key.equals("receive_mode")) {
+            updateReinitEnabledState();
+        } else if (key.equals("demod_log")) {
+            updateLogfileEnabledState();
+        }
+    }
+
+    /**
+     * disable logfile selection if logging is disabled
+     */
+    private void updateLogfileEnabledState() {
+        SwitchPreference switchPref = (SwitchPreference) findPreference("demod_log");
+        EditTextPreference topPref = (EditTextPreference) findPreference("demod_top_log_path");
+        EditTextPreference botPref = (EditTextPreference) findPreference("demod_bot_log_path");
+        topPref.setEnabled(switchPref.isChecked());
+        botPref.setEnabled(switchPref.isChecked());
+    }
+
+    /**
+     * disable automatic reinit if manual mode is selected
+     */
+    private void updateReinitEnabledState() {
+        ListPreference listPref = (ListPreference) findPreference("receive_mode");
+        SwitchPreference switchPref = (SwitchPreference) findPreference("automatic_init");
+        if (listPref.getValue().equals("1"))
+            switchPref.setEnabled(false);
+        else
+            switchPref.setEnabled(true);
+    }
 }
