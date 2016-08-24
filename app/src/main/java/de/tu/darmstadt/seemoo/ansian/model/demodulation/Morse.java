cac06e2aad119fc369a932a2cf6fddd727ba32f0
Dennis Mantz <dennis.mantz@googlemail.com>
2016-08-24 12:29:00 +0200

    Bugfix: Initialize state variable in Morse to prefent NullPointerException because of race condition.

--- app/src/main/java/de/tu/darmstadt/seemoo/ansian/model/demodulation/Morse.java
+++ app/src/main/java/de/tu/darmstadt/seemoo/ansian/model/demodulation/Morse.java
@@ -38,7 +38,7 @@ public class Morse extends Demodulation {
     private boolean automaticReinit;
 
     private DemodPreference prefs;
-    private State state;
+    private State state = State.INIT;
     private Mode mode;
 
     // buffers for data that is used for dit duration calibration
