8f35fd221278e07728bf69066b00a4c1f9e15353
Matthias Kannwischer <m.kannwischer@web.de>
2016-11-03 11:09:59 +0100

    inital doc template. first costumization.

---
+++ docs/docu_v3_Kannwischer/clean.bat
@@ -0,0 +1,38 @@
+@echo off
+set filename=document
+
+del %filename%.acn
+del %filename%.acr
+del %filename%.alg
+del %filename%*.aux
+del %filename%*.bbl
+del %filename%*.blg
+del %filename%.glg
+del %filename%.glo
+del %filename%.gls
+del %filename%.ist
+del %filename%.lof
+del %filename%.log
+del %filename%.lop
+del %filename%.lot
+del %filename%.slg
+del %filename%.syg
+del %filename%.syi
+del %filename%.toc
+del %filename%.url
+del %filename%.loalg
+del %filename%.lol
+
+del %filename%.out
+
+del %filename%.run.xml
+del %filename%.synctex.gz
+
+del btaux.aux
+del btaux.bbl
+del btaux.blg
+del btbbl.aux
+del btbbl.bbl
+del btbbl.blg
+
+del chap\*.aux
